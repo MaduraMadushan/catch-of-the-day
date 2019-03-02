b996eba95079b180199ab796484f3caf08940b3e
MaduraMadushan <45748038+MaduraMadushan@users.noreply.github.com>
2019-03-02 16:12:24 +0530

    Add StorePicker component

--- src/components/StorePicker/StorePicker.js
+++ src/components/StorePicker/StorePicker.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { getFunName } from "./../../utils/helpers";
 
-const StorePicker = () => {
-  return <div>StorePicker</div>;
+const StorePicker = props => {
+  const storeInput = React.createRef();
+  const goToStore = event => {
+    event.preventDefault();
+    const storeName = storeInput.current.value;
+    props.history.push(`/store/${storeName}`);
+  };
+  return (
+    <form onSubmit={goToStore}>
+      <h2>Please Enter A Store</h2>
+      <input
+        type="text"
+        required
+        defaultValue={getFunName()}
+        placeholder="Store Name"
+        ref={storeInput}
+      />
+      <button type="submit">Visit Store</button>
+    </form>
+  );
 };
 
 export default StorePicker;
