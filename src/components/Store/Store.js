904c9f2025d4a73573fc26cbc75c2cd7abd3abe5
MaduraMadushan <45748038+MaduraMadushan@users.noreply.github.com>
2019-03-02 16:22:45 +0530

    setting up store components

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
+import Header from "./Header";
+import Fish from "./Fish";
+import Order from "./Order";
+import Inventory from "./Inventory/Inventory";
+
 class Store extends Component {
   render() {
-    return <div>Store</div>;
+    return (
+      <div>
+        <Header />
+        <Fish />
+        <Order />
+        <Inventory />
+      </div>
+    );
   }
 }
 
